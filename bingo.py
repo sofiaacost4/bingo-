4d32fd9620f6f7224b4a0239aa57ebf86c984296
sofia <k.sofia@escolar.ifrn.edu.br>
2025-01-26 23:01:31 -0300

    "segundo commit"

---
+++ bingo.py
@@ -0,0 +1,13 @@
+import random
+#Aluna: Sofia Costa
+#Turma: Infoweb 1 ano
+#Professor: Alexandre
+
+#constantes (não devem mudar ao decorrer do jogo)
+
+num_linhasr = 2
+num_linhasd = 3
+num_colunasr = 3
+num_colunasd = 4
+num_cartelasr = 2
+num_cartelasd = 4
